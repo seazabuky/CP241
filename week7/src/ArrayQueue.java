3e8086e7a0eb79810d4f9ad4497b5ccecc20cd5e
saltswater <81177696+seazabuky@users.noreply.github.com>
2022-09-14 17:11:00 +0700

    Add files via upload

---
+++ week7/src/ArrayQueue.java
@@ -0,0 +1,72 @@
+public class ArrayQueue implements QueueADT{
+    private int total;
+    private Object s[];
+    int front,rear;
+    public ArrayQueue(){
+        Queue();
+    }
+    @Override
+    public void Queue() {
+        front = -1;
+        rear = -1;
+        total = 100;
+        s = new Object[total];
+    }
+
+    @Override
+    public void enqueue(Object e) throws Exception {
+        if (isFull()) {
+            throw new Exception("Queue is full");
+        }else{
+            rear = (rear+1)%total;
+            s[rear] = e;
+            if(front == -1){
+                front = 0;
+            }
+        }
+    }
+
+    @Override
+    public Object dequeue() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }else{
+            int removeitem = front;
+            if(front==rear)
+                front=rear=-1;
+            else
+                front=(front+1)%total;
+            return s[removeitem];
+            
+        }
+    }
+    @Override
+    public Object front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }else{
+            return s[front];
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return rear == -1;
+    }
+
+    @Override
+    public boolean isFull() {
+        return rear==total;
+    }
+
+    @Override
+    public void clear() {
+        front=rear=-1;
+    }
+
+    @Override
+    public int length() {
+        return rear+1;
+    }
+    
+}
---
+++ week7/src/QueueTest.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+public class QueueTest {
+    public static void main(String[] args) throws Exception {
+        String choice;
+        int ch = 0;
+        Object obj;
+        Student e;
+
+        ArrayQueue queue = new ArrayQueue();
+        //LinkedQueue queue = new LinkedQueue();
+        while(ch != 5){
+            try{
+                choice = JOptionPane.showInputDialog(null,
+                "------------------------------------ \n"+
+                "                 Menu \n"+
+                "------------------------------------ \n"+
+                "       1. Enqueue \n"+
+                "       2. Dequeue \n"+
+                "       3. Front \n"+
+                "       4. Clear \n"+
+                "       5. Exit \n"+
+                "------------------------------------ \n");
+                ch = Integer.parseInt(choice);
+                switch(ch){
+                    case 1:
+                        e = getElement();
+                        obj=e;
+                        queue.enqueue(obj);
+                        break;
+                    case 2:
+                        obj = queue.dequeue();
+                        e = (Student)obj;
+                        printElement(e);
+                        break;
+                    case 3:
+                        obj = queue.front();
+                        e = (Student)obj;
+                        printElement(e);
+                        break;
+                    case 4:
+                        queue.clear();
+                        JOptionPane.showMessageDialog(null, "Queue is cleared");
+                        break;
+                    case 5:
+                        JOptionPane.showMessageDialog(null, "Goodbye");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Invalid choice");
+                        break;
+                }
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+                System.exit(0);
+                }
+            }
+        }
+        
+        public static Student getElement(){
+            Student temp;
+            String idS = JOptionPane.showInputDialog(null,"Please input ID: ");
+            String nameS = JOptionPane.showInputDialog(null,"Please input name: ");
+            double midS = Double.parseDouble(JOptionPane.showInputDialog(null,"Please input Midterm score: "));
+            double finalS = Double.parseDouble(JOptionPane.showInputDialog(null,"Please input Final score: "));
+            double quizS = Double.parseDouble(JOptionPane.showInputDialog(null,"Please input Quiz score: "));
+            double prjS = Double.parseDouble(JOptionPane.showInputDialog(null,"Please input Project score: "));
+            double collabS = Double.parseDouble(JOptionPane.showInputDialog(null,"Please input Collaboration score: "));
+            temp = new Student();
+            temp.setStudent(idS, nameS, midS, finalS, quizS, prjS, collabS);
+            return temp;
+        }
+        
+        public static void printElement(Student e){
+            if (e != null){
+                JOptionPane.showMessageDialog(null,
+                 "ID: "+e.getId()+
+                 "\nName: "+e.getName()+
+                 "\nMidterm score: "+e.getMidScore()+
+                 "\nFinal score: "+e.getFinalScore()+
+                 "\nQuiz score: "+e.getQuizScore()+
+                 "\nProject score: "+e.getPrjScore()+
+                 "\nCollaboration score: "+e.getCollabScore());
+            }
+        }
+
+}
